9a3a51cc6ecf8f9b958d0235aae537910d9e0d8e
Neal Wicke <neal.wicke@hotmail.com>
2021-05-11 22:39:09 +0200

    Client login part1

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,17 +24,14 @@ namespace API
         {
             _config = config;
         }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices(_config);
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
             services.AddCors();
             services.AddIdentityServices(_config);
+            services.AddAuthorization();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +40,6 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
